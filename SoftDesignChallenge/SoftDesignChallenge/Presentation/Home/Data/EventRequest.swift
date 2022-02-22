c9d0bf0768de04e7f6753897e9387d1b1203c13f
Daniel Leal de Sousa <daniel.sousa-terceiro@cea.com.br>
2022-02-22 17:26:17 -0300

    Creating event requests

---
+++ SoftDesignChallenge/SoftDesignChallenge/Presentation/Home/Data/EventRequest.swift
@@ -0,0 +1,57 @@
+//
+//  EventRequest.swift
+//  SoftDesignChallenge
+//
+//  Created by ACT on 22/02/22.
+//
+
+import Foundation
+
+enum EventRequest: Request {
+  case getEvents
+  case getEvent (id: Int)
+  case checkIn (model: CheckIn)
+  
+  var scheme: String {
+    return "http"
+  }
+  
+  var host: String {
+    return "http://5f5a8f24d44d640016169133.mockapi.io/api/"
+  }
+  
+  var path: String {
+    switch self {
+    case .getEvents:
+      return "events/"
+    case .getEvent (let id):
+      return "events/\(id)"
+    case .checkIn:
+      return "checkin"
+    }
+  }
+  
+  var method: HTTPMethod {
+    switch self {
+    case .getEvents, .getEvent:
+      return .get
+    case .checkIn:
+      return .post
+    }
+  }
+  
+  var headers: [String : String] {
+    return [:]
+  }
+  
+  var params: [String : Any]? {
+    switch self {
+    case .getEvents, .getEvent:
+      return nil
+    case .checkIn(let model):
+      return model.dictionary
+    }
+  }
+  
+  
+}
---
+++ SoftDesignChallenge/SoftDesignChallenge/Presentation/Home/Data/Models/CheckIn.swift
@@ -0,0 +1,14 @@
+//
+//  CheckIn.swift
+//  SoftDesignChallenge
+//
+//  Created by ACT on 22/02/22.
+//
+
+import Foundation
+
+struct CheckIn: Encodable {
+  let eventId: Int
+  let name: String
+  let email: String
+}
