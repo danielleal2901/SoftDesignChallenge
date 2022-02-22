0ad9797f241896cd1f6299478a29a73d5dc110f8
Daniel Leal de Sousa <daniel.sousa-terceiro@cea.com.br>
2022-02-22 15:19:54 -0300

    Setting architecture configuration

---
+++ SoftDesignChallenge/SoftDesignChallenge/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoftDesignChallenge
+//
+//  Created by ACT on 22/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
